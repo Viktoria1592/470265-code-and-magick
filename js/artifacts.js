aa5efe2484b7ef167f2317c88399480df10d22e2
Maksim Kalnik <kalnikmaxim@gmail.com>
2018-02-21 15:39:44 +0200

    module5-task2

---
+++ js/artifacts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function () {
+  var shopElement = window.dialog.popup.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      artifactsElement.style.outline = '2px dashed red';
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+  });
+
+  var artifactsElement = document.querySelector('.setup-artifacts');
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    artifactsElement.style.outline = 'none';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    artifactsElement.style.outline = '2px dashed red';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragend', function (evt) {
+    artifactsElement.style.outline = 'none';
+    evt.preventDefault();
+  });
+
+})();
