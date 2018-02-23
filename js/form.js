caa2892fcaa96a3da7119b2b3dbd3b4ce9f80804
Maksim Kalnik <kalnikmaxim@gmail.com>
2018-02-23 14:47:10 +0200

    module6-task2 temp-2 backend finish

--- js/form.js
+++ js/form.js
@@ -1,11 +1,11 @@
 'use strict';
 
-(function() {
+(function () {
   var form = document.querySelector('.setup-wizard-form');
   var userDialog = document.querySelector('.setup-similar');
 
   form.addEventListener('submit', function (evt) {
-    window.save.upload(new FormData(form), function () {
+    window.backend.save(new FormData(form), function () {
       userDialog.classList.add('hidden');
     }, window.backend.errorHandler);
     evt.preventDefault();
